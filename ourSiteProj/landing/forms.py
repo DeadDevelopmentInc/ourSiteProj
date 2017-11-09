bae33e1e4543693beef80c0a783fb6ad87f8f2e4
Dead Development Inc <demidov15alexsand@gmail.com>
2017-11-09 12:39:39 +0300

    Add login form

--- ourSiteProj/landing/forms.py
+++ ourSiteProj/landing/forms.py
@@ -1,5 +1,11 @@
-from django.forms import CharField, Form, PasswordInput
-from django import forms
 
-class RegistationForm(Form):
-    
+from django.contrib.auth.forms import UserCreationForm
+
+class SignUpForm(UserCreationForm):
+    first_name = forms.CharField(max_length=30, required=False, help_text='Optional.')
+    last_name = forms.CharField(max_length=30, required=False, help_text='Optional.')
+    email = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.')
+
+    class Meta:
+        model = User
+        fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2', )
